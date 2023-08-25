10f5f5fbe46cdd264a9fe607c2b0ea80be3f62e7
luxayo <luciolumenaya@gmail.com>
2023-08-26 00:07:11 +0700

    adding delete and favorite

--- src/pages/contact/index.page.tsx
+++ src/pages/contact/index.page.tsx
@@ -1,13 +1,16 @@
-"use client";
 import { useRouter } from "next/router";
 import Divider from "@components/divider.component";
 import FloatingButton from "@components/floating-button.component";
 import SearchBar from "@components/search-bar.component";
 import {
   GetContactListQuery,
+  useDeleteContactMutation,
   useGetContactListLazyQuery,
 } from "@api/generated";
 import { useEffect, useState } from "react";
+import StarIcon from "@mui/icons-material/Star";
+import StarOutlineIcon from "@mui/icons-material/StarOutline";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 const Contact = () => {
   const router = useRouter();
@@ -15,9 +18,12 @@ const Contact = () => {
   const [offset, setOffset] = useState(0);
   const [search, setSearch] = useState("");
 
-  const [getContact, { loading, error }] = useGetContactListLazyQuery();
+  const [getContact] = useGetContactListLazyQuery();
+
+  const [removeContact, { loading, error }] = useDeleteContactMutation();
 
   const [phoneList, setPhoneList] = useState<GetContactListQuery | undefined>();
+  const [phoneFavoriteList, setPhoneFavoriteList] = useState<number[]>([]);
 
   useEffect(() => {
     if (!localStorage.getItem("phone")) {
@@ -32,6 +38,15 @@ const Contact = () => {
       console.log("ambil");
       const data = localStorage.getItem("phone");
       if (data) setPhoneList(JSON.parse(data));
+    }
+    if (localStorage.getItem("favorite")) {
+      const data = localStorage.getItem("favorite");
+      if (data) {
+        console.log("ambil fav");
+        const favorite = JSON.parse(data) as number[];
+        console.log(favorite);
+        setPhoneFavoriteList(favorite);
+      }
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
@@ -66,6 +81,35 @@ const Contact = () => {
         setPhoneList(temp);
       }
     });
+  };
+
+  const handleAddFavorite = (id: number) => {
+    const favorite = [...phoneFavoriteList];
+    favorite.push(id);
+    setPhoneFavoriteList(favorite);
+    console.log(favorite);
+    localStorage.setItem("favorite", JSON.stringify(favorite));
+  };
+  const handleRemoveFavorite = (id: number) => {
+    const favorite = phoneFavoriteList;
+    const filtered = favorite.filter((value) => value !== id);
+    setPhoneFavoriteList(filtered);
+    console.log(filtered);
+    localStorage.setItem("favorite", JSON.stringify(filtered));
+  };
+
+  const handleDeleteContact = (id: number) => {
+    removeContact({ variables: { id: id } })
+      .then((response) => {
+        const data = localStorage.getItem("phone");
+        if (data) {
+          const phone = JSON.parse(data) as GetContactListQuery;
+          const filtered = phone.contact.filter((value) => value.id !== id);
+          localStorage.setItem("phone", JSON.stringify({ contact: filtered }));
+          setPhoneList({ contact: filtered });
+        }
+      })
+      .catch((error) => alert(error.message));
   };
 
   const renderPhoneNumber = (
@@ -116,49 +160,134 @@ const Contact = () => {
           maxWidth: "800px",
         }}
       >
-        {phoneList ? (
+        <p>Favorite</p>
+        {phoneList && phoneFavoriteList.length > 0 ? (
           phoneList.contact
             .filter(
               (phone) =>
-                phone.first_name.toLowerCase().includes(search) ||
-                phone.last_name.toLowerCase().includes(search) ||
-                (phone.phones !== undefined
-                  ? phone.phones.find((number) =>
-                      number.number.includes(search)
-                    )
-                  : false)
+                phoneFavoriteList.includes(phone.id) &&
+                (phone.first_name.toLowerCase().includes(search) ||
+                  phone.last_name.toLowerCase().includes(search) ||
+                  (phone.phones !== undefined
+                    ? phone.phones.find((number) =>
+                        number.number.includes(search)
+                      )
+                    : false))
             )
             .map((item, key) => (
               <div
                 css={{
                   boxSizing: "border-box",
                   display: "flex",
-                  flexDirection: "column",
+                  flexDirection: "row",
                   justifyContent: "center",
                   cursor: "pointer",
+                  width: "100%",
                 }}
                 key={key}
-                onClick={() => handleContactButton(item.id)}
               >
                 <div
                   css={{
                     boxSizing: "border-box",
+                    display: "flex",
+                    flex: 1,
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    cursor: "pointer",
                   }}
+                  onClick={() => handleContactButton(item.id)}
                 >
-                  {item.first_name}
+                  <div
+                    css={{
+                      boxSizing: "border-box",
+                    }}
+                  >
+                    {item.first_name}
+                  </div>
+                  <div
+                    css={{
+                      boxSizing: "border-box",
+                    }}
+                  >
+                    {renderPhoneNumber(item.phones)}
+                  </div>
                 </div>
-                <div
-                  css={{
-                    boxSizing: "border-box",
-                  }}
-                >
-                  {renderPhoneNumber(item.phones)}
-                </div>
+                <button onClick={() => handleRemoveFavorite(item.id)}>
+                  <StarIcon />
+                </button>
+                <button onClick={() => handleDeleteContact(item.id)}>
+                  <DeleteIcon />
+                </button>
                 <Divider />
               </div>
             ))
         ) : (
-          <span />
+          <p> No Favorite Yet :&#40;</p>
+        )}
+
+        <p>Contact List</p>
+        {phoneList ? (
+          phoneList.contact
+            .filter(
+              (phone) =>
+                !phoneFavoriteList.includes(phone.id) &&
+                (phone.first_name.toLowerCase().includes(search) ||
+                  phone.last_name.toLowerCase().includes(search) ||
+                  (phone.phones !== undefined
+                    ? phone.phones.find((number) =>
+                        number.number.includes(search)
+                      )
+                    : false))
+            )
+            .map((item, key) => (
+              <div
+                css={{
+                  boxSizing: "border-box",
+                  display: "flex",
+                  flexDirection: "row",
+                  justifyContent: "center",
+                  cursor: "pointer",
+                  width: "100%",
+                }}
+                key={key}
+              >
+                <div
+                  css={{
+                    boxSizing: "border-box",
+                    display: "flex",
+                    flex: 1,
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    cursor: "pointer",
+                  }}
+                  onClick={() => handleContactButton(item.id)}
+                >
+                  <div
+                    css={{
+                      boxSizing: "border-box",
+                    }}
+                  >
+                    {item.first_name}
+                  </div>
+                  <div
+                    css={{
+                      boxSizing: "border-box",
+                    }}
+                  >
+                    {renderPhoneNumber(item.phones)}
+                  </div>
+                </div>
+                <button onClick={() => handleAddFavorite(item.id)}>
+                  <StarOutlineIcon />
+                </button>
+                <button onClick={() => handleDeleteContact(item.id)}>
+                  <DeleteIcon />
+                </button>
+                <Divider />
+              </div>
+            ))
+        ) : (
+          <p />
         )}
 
         <button onClick={handleLoadMoreButton}>Load More</button>
